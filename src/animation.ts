7352969702d5ae362afc80ddb61ff973f5170a03
Marcin Majkowski <pl.marcin.m@gmail.com>
2018-06-17 11:31:13 +0200

    Add shapes from MDN tutorial

--- src/animation.ts
+++ src/animation.ts
@@ -24,18 +24,44 @@ export class Animation {
 
     this.ctx.fillStyle = 'blue';
     this.ctx.font = '30px Arial';
-    this.ctx.textAlign = 'left';
+    this.ctx.textAlign = 'start';
     this.ctx.fillText(`${this.canvas.width} x ${this.canvas.height}`, 100, 200);
     this.ctx.fillText(`${window.innerWidth} x ${window.innerHeight}`, 100, 300);
 
-    ctx.fillStyle = 'rgb(200, 0, 0)';
-    ctx.fillRect(10, 10, 50, 50);
-
-    ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
-    ctx.fillRect(30, 30, 50, 50);
+    ctx.strokeStyle = 'black';
+    this.drawSmile();
+    ctx.strokeStyle = 'green';
+    this.drawSpeechBalloon();
 
     this.fps.x = window.innerWidth / 2;
     this.fps.update();
     this.fps.draw();
+  }
+
+  private drawSmile(): void {
+    const ctx = this.ctx;
+    ctx.beginPath();
+    ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Outer circle
+    ctx.moveTo(110, 75);
+    ctx.arc(75, 75, 35, 0, Math.PI, false);  // Mouth (clockwise)
+    ctx.moveTo(65, 65);
+    ctx.arc(60, 65, 5, 0, Math.PI * 2, true);  // Left eye
+    ctx.moveTo(95, 65);
+    ctx.arc(90, 65, 5, 0, Math.PI * 2, true);  // Right eye
+    ctx.strokeStyle = 'red';
+    ctx.stroke();
+  }
+
+  private drawSpeechBalloon(): void {
+    const ctx = this.ctx;
+    ctx.beginPath();
+    ctx.moveTo(75, 25);
+    ctx.quadraticCurveTo(25, 25, 25, 62.5);
+    ctx.quadraticCurveTo(25, 100, 50, 100);
+    ctx.quadraticCurveTo(50, 120, 30, 125);
+    ctx.quadraticCurveTo(60, 120, 65, 100);
+    ctx.quadraticCurveTo(125, 100, 125, 62.5);
+    ctx.quadraticCurveTo(125, 25, 75, 25);
+    ctx.stroke();
   }
 }
